354246d1a13fc770ceab83c581dcd0e957466075
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-02-15 11:51:34 +0530

    Adding category model and removing the tag model

---
+++ models/Category.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('category', categorySchema);
